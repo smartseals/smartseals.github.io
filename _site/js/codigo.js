2537b127a9b2b0cba4460bf8f40653596e625352
Memo <alvaro@whiteideas.co>
2014-05-17 11:02:31 -0500

    prueba 4

--- _site/js/codigo.js
+++ _site/js/codigo.js
@@ -1,5 +1,11 @@
-var button = document.querySelector("button");
-//if a user click on it sey hello
-button.addEventListener("click",function(ev){
-  alert("hello");
-},false);
+var map;
+function initialize() {
+    var mapOptions = {
+        center: new google.maps.LatLng(11, -73),
+        zoom: 8
+        };
+    map = new google.maps.Map(document.getElementById("map-canvas"),
+            mapOptions);
+      }
+        
+google.maps.event.addDomListener(window, 'load', initialize);
