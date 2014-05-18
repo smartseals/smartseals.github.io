7ce982f08cfe1dfd9ba52f3fe5590121c2a8cd56
Memo <alvaro@whiteideas.co>
2014-05-18 01:12:07 -0500

    js en otro folder

--- _site/js/codigo.js
+++ _site/js/codigo.js
@@ -1,11 +1,8 @@
-var map;
-function initialize() {
-    var mapOptions = {
-        center: new google.maps.LatLng(11, -73),
-        zoom: 8
-        };
-    map = new google.maps.Map(document.getElementById("map-canvas"),
-            mapOptions);
-      }
-        
-google.maps.event.addDomListener(window, 'load', initialize);
+function initialize(){
+  var mapOptions =  {
+    center: new google.maps.LatLng(12,-72),
+    zoom: 8
+  }
+  var map = new google.maps.Map(document.getElementById("map-canvas"),mapOptions);
+}
+google.maps.event.addDomListener(window,'load',initialize);
