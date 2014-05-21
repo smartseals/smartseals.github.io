bba90a41263464802f02afa0bd8160103efd2c78
Memo <alvaro@whiteideas.co>
2014-05-20 23:21:37 -0500

    carga web de ultimo mapa

--- _site/js/codigo.js
+++ _site/js/codigo.js
@@ -1,9 +1,58 @@
-function initialize(){
-  var mapOptions =  {
-    center: new google.maps.LatLng(12,-72),
-    zoom: 8
-  }
-  var map = new google.maps.Map(document.getElementById("map-canvas"),mapOptions);
-}
-google.maps.event.addDomListener(window,'load',initialize);
+var map,
+    currentPositionMarker,
+    mapCenter = new google.maps.LatLng(10, -72),
+    map;
 
+function initLocationProcedure(){
+  initilizeMap();
+  if (navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(displayAndWatch, locError);
+  }
+  else{
+    alert("Your browser does not support the Geolocation API");
+  }
+}
+
+function initilizeMap(){
+  map = new google.maps.Map(document.getElementById('map-canvas'),{
+    zoom: 10,
+    center: mapCenter,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  });
+}
+
+function displayAndWatch(position){
+  setCurrentPosition(position);
+  watchCurrentPosition();
+}
+
+function setCurrentPosition(pos){
+  var position = new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude);
+  currentPositionMarker = new google.maps.Marker({
+    map: map,
+    position: position, 
+    title: "Current Position!"
+  });
+  map.panTo(position);
+}
+
+function watchCurrentPosition(){
+  var positionTimer = navigator.geolocation.watchPosition(
+    function(position){
+      setMarkerPosition(currentPositionMarker, position);
+  }); 
+}
+
+function setMarkerPosition(marker, position){
+  marker.setPosition(
+    new google.maps.LatLng(position.coords.latitude, position.coords.longitude)
+  );
+}
+
+function locError(){
+  alert("The current position could not be found");
+}
+
+$(document).ready(function(){
+  initLocationProcedure();
+});
