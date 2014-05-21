9eb96ca70fcec36c64c594e7f1b270ec3f0986ba
Memo <alvaro@whiteideas.co>
2014-05-21 01:26:49 -0500

    prueba final

--- js/codigo.js
+++ js/codigo.js
@@ -31,18 +31,39 @@ function setCurrentPosition(pos){
   currentPositionMarker = new google.maps.Marker({
     map: map,
     position: position, 
-    title: "Current Position!"
+    title: "Current Position!",
+    icon: getCircle()
   });
   map.panTo(position);
 }
 
+function getCircle(){
+  return{
+    path: google.maps.SymbolPath.CIRCLE,
+    fillColor: 'green',
+    fillOpacity: 0.8,
+    scale: 8,
+    strokeColor: 'red',
+    strokeWeight: 5,
+    strokeOpacity: 0.5 
+  };
+}
 
+var geo_options = {
+  enableHighAccuracy: true,
+  maximumAge: 15000,
+  timeout: 5000
+};
+
+function geo_error(){
+  alert("Sorry, no position available");
+}
 
 function watchCurrentPosition(){
   var positionTimer = navigator.geolocation.watchPosition(
     function(position){
       setMarkerPosition(currentPositionMarker, position);
-  }); 
+  }, geo_error, geo_options); 
 }
 
 function setMarkerPosition(marker, position){
